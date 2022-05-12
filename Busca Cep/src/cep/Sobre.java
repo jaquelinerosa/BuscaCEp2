76d3dafa0bc20b5782a29217d442b1296bd667f6
Jaqueline <59242839+jaquelinerosa@users.noreply.github.com>
2022-05-12 18:22:48 -0300

    Add files via upload

---
+++ Busca Cep/src/cep/Sobre.java
@@ -0,0 +1,93 @@
+package cep;
+
+import java.awt.Cursor;
+import java.awt.Desktop;
+import java.awt.EventQueue;
+import java.awt.SystemColor;
+import java.awt.Toolkit;
+import java.net.URI;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Sobre extends JDialog {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Sobre dialog = new Sobre();
+					dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+					dialog.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the dialog.
+	 */
+	public Sobre() {
+		setModal(true);
+		setResizable(false);
+		setTitle("Sobre");
+		setIconImage(Toolkit.getDefaultToolkit().getImage(Sobre.class.getResource("/img/home.png")));
+		setBounds(150, 150, 450, 300);
+		getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Buscar CEP - vers\u00E3o 1.0");
+		lblNewLabel.setForeground(SystemColor.textHighlight);
+		lblNewLabel.setBounds(37, 42, 175, 14);
+		getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Jaqueline Rosa");
+		lblNewLabel_1.setForeground(SystemColor.textHighlight);
+		lblNewLabel_1.setBounds(37, 90, 122, 14);
+		getContentPane().add(lblNewLabel_1);
+		
+		JButton btnGithub = new JButton("");
+		btnGithub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				link("https://github.com/jaquelinerosa");
+			}
+		});
+		btnGithub.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				link("https://github.com/jaquelinerosa");
+			}
+		});
+		btnGithub.setToolTipText("GitHub");
+		btnGithub.setIcon(new ImageIcon(Sobre.class.getResource("/img/github.png")));
+		btnGithub.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		btnGithub.setBorder(null);
+		btnGithub.setBackground(SystemColor.control);
+		btnGithub.setBounds(37, 191, 48, 48);
+		getContentPane().add(btnGithub);
+		
+		
+
+	}//fim do construtor
+	private void link(String site) {
+		Desktop desktop =  Desktop.getDesktop();
+		try {
+			URI uri = new URI(site);
+			desktop.browse(uri);
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
+	
+
+}
